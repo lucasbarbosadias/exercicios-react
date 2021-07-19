9db79e051c9c442cc5f9022b61ab40ab3835897d
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-07-19 10:09:00 -0300

    chore: declaring the function on the map

--- mobile-tab-navigation/src/components/Navbar/index.js
+++ mobile-tab-navigation/src/components/Navbar/index.js
@@ -7,13 +7,8 @@ const Nav = () => {
 
   const [component, setComponent] = useState('home')
 
-  const activeHome = () => setComponent('home')
-  const activeWork = () => setComponent('work')
-  const activeBlog = () => setComponent('blog')
-  const activeAbout = () => setComponent('about')
-
   useEffect(() => {
-    console.log("ATUALIZOU", component)
+    console.log(component, 'foi Atualizado')
   }, [component])
 
   return(
@@ -22,30 +17,14 @@ const Nav = () => {
       {component === 'work' && (<Work />)}
       {component === 'blog' && (<Blog />)}
       {component === 'about' && (<About />)}
+      
       <ul className="menu">
-        <li onClick={activeHome}>
-          <i className="fas fa-home" />
-          <p>Home</p>
-        </li>
-        <li onClick={activeWork}>
-          <i className="fas fa-box" />
-          <p>Work</p>
-        </li>
-        <li onClick={activeBlog}>
-          <i className="fas fa-book-open" />
-          <p>Blog</p>
-        </li>
-        <li onClick={activeAbout}>
-          <i className="fas fa-users" />
-          <p>About Us</p>
-        </li>
-        {/* onClick espera uma função, mas da erro pois recebe uma string :/
         {MENU.map((item) => (
-          <li key={item.id} onClick={item.func}>
+          <li key={item.id} onClick={() => setComponent(item.func)}>
             <i className={item.icon} />
             <p>{item.text}</p>
           </li>
-        ))} */}
+        ))}
       </ul>
     </>
   )
